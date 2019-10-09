e5d28253d4de712be0a8b4c7941cb9100dd1eca7
root <ybasori@gmail.com>
2019-10-09 20:51:57 +0700

    perbaikan bottom nav

--- App.js
+++ App.js
@@ -103,20 +103,46 @@ const ProfileStack = createStackNavigator({
 const MainNavigator = createAppContainer(createBottomTabNavigator({
       ForYou: {
         screen: ForYouStack,
-        navigationOptions:{
-          title: "For You",
-          tabBarIcon: ({ tintColor }) => (
-            <Icon type="FontAwesome" name="tablet" />
-          )
-        }
-      },
+        navigationOptions:({navigation})=>{
+            var noBottomTabNav = [
+              "DetailTitle",
+              "DetailEpisode"
+            ];
+            var obj = {
+              title: 'For You',
+              tabBarIcon: ({ tintColor }) => (
+                <Icon type="FontAwesome" name="tablet" />
+              )
+            };
+            if(noBottomTabNav.indexOf(navigation.state.routes[navigation.state.index].routeName) >= 0){
+              obj.tabBarVisible = false;
+            }
+            else{
+              obj.tabBarVisible = true;
+            }
+            return obj
+          }
+        },
       Favorite: {
           screen: FavoriteStack,
-          navigationOptions:{
-            title: 'Favorite',
-            tabBarIcon: ({ tintColor }) => (
-              <Icon type="FontAwesome" name="star" />
-            )
+          navigationOptions:({navigation})=>{
+            var noBottomTabNav = [
+              "DetailTitle",
+              "DetailEpisode"
+            ];
+            var obj = {
+              title: 'Favorite',
+              tabBarIcon: ({ tintColor }) => (
+                <Icon type="FontAwesome" name="star" />
+              )
+            };
+            if(noBottomTabNav.indexOf(navigation.state.routes[navigation.state.index].routeName) >= 0){
+              obj.tabBarVisible = false;
+            }
+            else{
+              obj.tabBarVisible = true;
+            }
+            return obj
           }
       },
       Profile: {
