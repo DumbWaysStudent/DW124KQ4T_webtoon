dcf0d197120cb1b85e1bf1da1cad79e5e6122557
root <ybasori@gmail.com>
2019-10-09 10:02:55 +0700

    profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import { Container, Content, Item, Input, Card, CardItem, Body, Button, Text, H1, Icon } from 'native-base';
+import { Container, Content, Item, Input, Card, CardItem, Body, Button, Text, H1, Icon,View } from 'native-base';
+
+import { Image } from 'react-native';
+
+const profile = {
+  image: "https://avatars3.githubusercontent.com/u/18370818?s=460&v=4",
+  name: "Yusuf Basori"
+}
 
 
 
@@ -9,13 +16,27 @@ class ProfileScreen extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state = {
+          profile: profile
+        }
     }
 
     
 
   render() {
     return (
-        <Text>Profile</Text>
+        <Container>
+          <Content>
+            <Card>
+              <CardItem>
+                <Body style={{ flex: 1,alignItems:'center'}}>
+                  <Image style={{width:100, height: 100, borderRadius: 50}} source={{uri: this.state.profile.image}} />
+                </Body>
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
     );
   }
 };
