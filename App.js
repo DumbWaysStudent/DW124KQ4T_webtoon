bd6512d92a5fe698a203c1a046be5c690888bbc0
root <ybasori@gmail.com>
2019-10-11 08:43:30 +0700

    apa kek

--- App.js
+++ App.js
@@ -10,20 +10,12 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import LoginScreen from './src/screens/LoginScreen';
-import MainScreen from './src/screens/MainScreen';
 
 const AppNavigator = createStackNavigator({
   Login: {
     screen: LoginScreen,
     navigationOptions:{
       headerTransparent: true
-    }
-  },
-  Main: {
-    screen: MainScreen,
-    navigationOptions: {
-      headerTransparent: true,
-      headerLeft: null
     }
   }
 },
